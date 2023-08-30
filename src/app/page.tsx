67837b563186f69a8a46317515b7bf0a761ab6ee
Sanjeev Gunasekaran <sanjeev@bassure.com>
2023-08-30 17:43:10 +0530

    Build version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import styles from './page.module.css';
-
+import React from "react";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-      <section className={styles.main} style={{backgroundColor:"#020202"}}>
-          <img src="./rdr2.jpg" alt="rdr2" style={{width:"100%", height:"100%", objectFit:"cover"}}/>
-    
-          <div className={styles.center}  >
-              
-          
-              <h1>{`Welcome to Sanjeev's worst app`}</h1>
+    <section className={styles.main} style={{ backgroundColor: "#020202" }}>
+      <img
+        src="./rdr2.jpg"
+        alt="rdr2"
+        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+      />
+
+      <div className={styles.center}>
+        <h1>{`Welcome to Sanjeev's worst app`}</h1>
         <div className={styles.linkContainer}>
-          <a href="http://localhost:3000/hello" className={styles.link}>
+          <a href="http://localhost:3000/home" className={styles.link}>
             Some other stuff
           </a>
           {/* <a href="https://www.example.com" className={styles.link}>
@@ -21,9 +22,6 @@ export default function Home() {
           <a href="https://www.example.com" className={styles.link}>
             Link 3
           </a> */}
-
-         
-                  
         </div>
       </div>
     </section>
